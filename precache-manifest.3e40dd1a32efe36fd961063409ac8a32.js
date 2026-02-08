336c7f6476076a852c39e5bd7943a0f881b7a2d1
Md. Shaidur Rahaman Tanzid <mdshaidurrahamantanzid@gmail.com>
2026-02-08 13:28:18 +0600

    Updates

---
+++ precache-manifest.3e40dd1a32efe36fd961063409ac8a32.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36891a90da10dc797420a4fabe52a5df",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "0a27bc125c6e72e6d8da",
+    "url": "/Portfolio/static/css/2.e48ad200.chunk.css"
+  },
+  {
+    "revision": "d01cb2c6d825d6425785",
+    "url": "/Portfolio/static/css/main.44099544.chunk.css"
+  },
+  {
+    "revision": "0a27bc125c6e72e6d8da",
+    "url": "/Portfolio/static/js/2.863e5b8f.chunk.js"
+  },
+  {
+    "revision": "d01cb2c6d825d6425785",
+    "url": "/Portfolio/static/js/main.ed0802a9.chunk.js"
+  },
+  {
+    "revision": "6599c2232e09974824d1",
+    "url": "/Portfolio/static/js/runtime~main.b1f09880.js"
+  },
+  {
+    "revision": "3c80662bf8b54902e49a2fd1c5cd7833",
+    "url": "/Portfolio/static/media/banner.3c80662b.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6dd00b0ead66a555e28b8556a37579e7",
+    "url": "/Portfolio/static/media/review1.6dd00b0e.jpeg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
